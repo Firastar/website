35ebd871ced80923109687d41d70d5a159195e1e
Mohsen <mohsenando@gmail.com>
2022-08-21 23:31:12 +0430

    fix: add compare images

--- components/CompareSlider/CompareSlider.component.tsx
+++ components/CompareSlider/CompareSlider.component.tsx
@@ -1,13 +1,13 @@
 import React, { memo } from "react";
 import classes from "./CompareSlider.module.scss";
-import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
 import { ReactCompareSlider, styleFitContainer } from "react-compare-slider";
 import { LeftAngle, RightAngle } from "@svgs";
 import Image from "next/future/image";
+import { useTheme } from "next-themes";
 
 const CompareSlider = () => {
-  const { locale } = useRouter();
+  const { theme } = useTheme();
   const { t } = useTranslation("slider");
 
   return (
@@ -25,13 +25,13 @@ const CompareSlider = () => {
           }
           itemOne={
             <Image
-              src="/images/decorative-pic.jpg"
-              width={800}
-              height={800}
+              src={`/images/after-${theme || "light"}.webp`}
+              width={2000}
+              height={2000}
+              quality="100%"
               loading={"eager"}
               alt="Image-one"
               style={{
-                filter: locale === "fa" ? "grayscale(0)" : "grayscale(1)",
                 width: "100%",
                 height: "auto",
                 ...styleFitContainer(),
@@ -40,13 +40,13 @@ const CompareSlider = () => {
           }
           itemTwo={
             <Image
-              src="/images/decorative-pic.jpg"
+              src={`/images/before-${theme || "light"}.webp`}
+              quality="100%"
               alt="Image-two"
-              width={800}
-              height={800}
+              width={2000}
+              height={2000}
               loading={"eager"}
               style={{
-                filter: locale === "fa" ? "grayscale(1)" : "grayscale(0)",
                 width: "100%",
                 height: "auto",
                 ...styleFitContainer(),
@@ -72,13 +72,13 @@ const CompareSlider = () => {
           }
           itemOne={
             <Image
-              src="/images/decorative-pic2.jpg"
+              src={`/images/after-${theme || "light"}-mobile.webp`}
+              quality="100%"
               alt="Image-one"
               width={500}
               height={500}
               loading={"eager"}
               style={{
-                filter: "grayscale(1)",
                 width: "100%",
                 height: "auto",
                 ...styleFitContainer(),
@@ -87,13 +87,13 @@ const CompareSlider = () => {
           }
           itemTwo={
             <Image
-              src="/images/decorative-pic2.jpg"
+              src={`/images/before-${theme || "light"}-mobile.webp`}
               alt="Image-two"
+              quality="100%"
               width={500}
               height={500}
               loading={"eager"}
               style={{
-                filter: "grayscale(0)",
                 width: "100%",
                 height: "auto",
                 ...styleFitContainer(),
